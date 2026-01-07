af4563ab57a2d6dd0cd4ccea0934a7f27fc37a26
hamzazafar-dev <zrajahamza5@gmail.com>
2026-01-07 08:06:39 +0000

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by AppleMacbook on 07/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
